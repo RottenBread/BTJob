e233158ce42438d4ae8d4feb4b63007076619e6c
Junhyeok Moon <mig070@naver.com>
2025-08-20 20:17:07 +0900

    Skript 호환성 추가

--- src/main/kotlin/org/rottenbread/bTJob/api/GetLevel.kt
+++ src/main/kotlin/org/rottenbread/bTJob/api/GetLevel.kt
@@ -5,18 +5,20 @@ import org.bukkit.event.Listener
 import org.rottenbread.bTJob.data.DataSave
 
 class GetLevel: Listener {
-    fun getFarmer(player: Player): Int {
-        val farmer = DataSave("farmer")
-        return farmer.loadLV(player)
-    }
+    companion object {
+        fun getFarmer(player: Player): Int {
+            val farmer = DataSave("farmer")
+            return farmer.loadLV(player)
+        }
 
-    fun getMiner(player: Player): Int {
-        val miner = DataSave("miner")
-        return miner.loadLV(player)
-    }
+        fun getMiner(player: Player): Int {
+            val miner = DataSave("miner")
+            return miner.loadLV(player)
+        }
 
-    fun getFisher(player: Player): Int {
-        val fisher = DataSave("fisher")
-        return fisher.loadLV(player)
+        fun getFisher(player: Player): Int {
+            val fisher = DataSave("fisher")
+            return fisher.loadLV(player)
+        }
     }
 }
