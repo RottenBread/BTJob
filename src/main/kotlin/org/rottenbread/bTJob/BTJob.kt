6e6adff20a57f14532fe2d971435ee617e484166
Junhyeok Moon <mig070@naver.com>
2025-08-20 20:27:38 +0900

    롤백

--- src/main/kotlin/org/rottenbread/bTJob/BTJob.kt
+++ src/main/kotlin/org/rottenbread/bTJob/BTJob.kt
@@ -32,19 +32,19 @@ class BTJob : JavaPlugin(), Listener, CommandExecutor {
         if (command.name.equals("job", ignoreCase = true)) {
             if (sender is Player) {
                 if (sender.isOp) {
-
+                    val api = GetLevel()
                     if (args.isNotEmpty()) {
                         when (args[0].lowercase()) {
                             "miner" -> {
-                                sender.sendMessage("Your Miner Level: §7${GetLevel.getMiner(sender)}")
+                                sender.sendMessage("Your Miner Level: §7${api.getMiner(sender)}")
                             }
 
                             "fisher" -> {
-                                sender.sendMessage("Your Fisher Level: §7${GetLevel.getFisher(sender)}")
+                                sender.sendMessage("Your Fisher Level: §7${api.getFisher(sender)}")
                             }
 
                             "farmer" -> {
-                                sender.sendMessage("Your Farmer Level: §7${GetLevel.getFarmer(sender)}")
+                                sender.sendMessage("Your Farmer Level: §7${api.getFarmer(sender)}")
                             }
                         }
                     }
